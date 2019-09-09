3c848783457d021edf5cac822b33fd3d4890fde4
Aleksandr Kravtsov <tdr073@gmail.com>
2019-09-09 17:36:32 +0300

    Create BD1.php

---
+++ BD1.php
@@ -0,0 +1,10 @@
+<?php
+
+$link = mysqli_connect('localhost','root','','quest');
+
+	if(mysqli_connect_errno())
+	{
+		echo 'ERROR ('.mysqli_connect_errno().') : '. mysqli_connect_error()	;
+		exit();
+	}
+?>
