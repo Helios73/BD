b22d2535174e61b64a402557fd381bc3082ff372
Aleksandr Kravtsov <tdr073@gmail.com>
2019-09-13 17:57:32 +0300

    Modified delete.php

--- delete.php
+++ delete.php
@@ -6,7 +6,7 @@ $link = mysqli_connect('localhost','root','','quest');
 		exit();
 	}
 if (!empty($_REQUEST['form_submit']) && $_REQUEST['form_submit'] == 'Y') {
-  $result = mysqli_query("DELETE FROM `users` WHERE Phone = two VALUES ('".$_REQUEST['name']."','".$_REQUEST['phone']."','".$_REQUEST['email']."')");
+  $result = mysqli_query("DELETE FROM `users` VALUES ('".$_REQUEST['name']."','".$_REQUEST['phone']."','".$_REQUEST['email']."') WHERE Phone = two");
   if ($result) {
       echo "Успех";
   } else {
